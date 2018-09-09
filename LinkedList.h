291c89533e3d8c071ca7137fa4f14253a5b0b7d0
Konstantinos Giannakakis <dit16028@uop.gr>
2018-09-09 22:58:03 +0300

    Define basic list functions

--- LinkedList.h
+++ LinkedList.h
@@ -1,8 +1,11 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
+typedef void* Node;
+typedef void* LinkedList;
 
-
-
+Node* ListInsert(LinkedList* list, int new_value);
+Node* ListFindNode(LinkedList* list, int value);
+Node* ListRemove(LinkedList* list, int length);
 
 #endif
