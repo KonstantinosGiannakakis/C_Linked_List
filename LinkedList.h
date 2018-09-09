672d43947b618b73bff9c2b87e372f8d65165700
Konstantinos Giannakakis <dit16028@uop.gr>
2018-09-09 22:45:32 +0300

    Define list types

--- LinkedList.h
+++ LinkedList.h
@@ -1,7 +1,21 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
+typedef struct __Node{
 
+int value;
+struct __Node* next;
+
+}Node;
+
+typedef struct{
+
+Node* head;
+Node* tail;
+
+int length;
+
+}LinkedList;
 
 
 
